bc22150df3b77b7ed9b9decafe8ee4407a99fb28
3nylar <adefilaadebukola39@gmail.com>
2026-02-23 15:12:04 +0100

    practice

--- React-Native/app/(tabs)/Practice.tsx
+++ React-Native/app/(tabs)/Practice.tsx
@@ -3,7 +3,7 @@ import { Image, ScrollView, StyleSheet, Text, View} from "react-native";
 
 export default function ProfileCard() {
     return (
-        <ScrollView style={{backgroundColor: 'lightgrey', height: '100%,'}}>
+        <ScrollView style={styles.container}>
             <View style={styles.card}>
                 <Image source={require('../../assets/images/favicon.png')} style={styles.profileImage} />
                 <Text style={styles.profileName}>John Doe</Text>
